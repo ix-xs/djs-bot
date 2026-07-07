4de9a19e12586c244b6bc91c9b34f23af4f46b0c
Ix-xs <poplomo@outlook.fr>
2026-07-07 13:58:18 +0200

    Create module to export UI components

---
+++ src/components/$.js
@@ -0,0 +1,50 @@
+module.exports = {
+  get ActionRow() {
+    return require("./ActionRow");
+  },
+  get Button() {
+    return require("./Button");
+  },
+  get Checkbox() {
+    return require("./Checkbox");
+  },
+  get CheckboxGroup() {
+    return require("./CheckboxGroup");
+  },
+  get Container() {
+    return require("./Container");
+  },
+  get File() {
+    return require("./File");
+  },
+  get FileUpload() {
+    return require("./FileUpload");
+  },
+  get Gallery() {
+    return require("./Gallery");
+  },
+  get Label() {
+    return require("./Label");
+  },
+  get Modal() {
+    return require("./Modal");
+  },
+  get Section() {
+    return require("./Section");
+  },
+  get SelectMenu() {
+    return require("./SelectMenu");
+  },
+  get Separator() {
+    return require("./Separator");
+  },
+  get TextDisplay() {
+    return require("./TextDisplay");
+  },
+  get TextInput() {
+    return require("./TextInput");
+  },
+  get Thumbnail() {
+    return require("./Thumbnail");
+  },
+};
