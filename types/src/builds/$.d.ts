cfdc127ee419aa3e76cdd5cca581cda478d7ca6a
Ix-xs <poplomo@outlook.fr>
2026-07-07 13:54:04 +0200

    Create $.d.ts

---
+++ types/src/builds/$.d.ts
@@ -0,0 +1,10 @@
+import Attachment = require("./Attachment");
+import Embed = require("./Embed");
+import Interaction = require("./Interaction");
+import MessageContextCommand = require("./MessageContextCommand");
+import Poll = require("./Poll");
+import SlashCommand = require("./SlashCommand");
+import Unixify = require("./Unixify");
+import UserContextCommand = require("./UserContextCommand");
+export { Attachment, Embed, Interaction, MessageContextCommand, Poll, SlashCommand, Unixify, UserContextCommand };
+//# sourceMappingURL=$.d.ts.map
