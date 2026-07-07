900009b1a7a884af93eb85889f947ae0e624d9cc
Ix-xs <poplomo@outlook.fr>
2026-07-07 13:57:21 +0200

    Create $ module to export various components

---
+++ src/builds/$.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+const Attachment = require("./Attachment");
+const Embed = require("./Embed");
+const Interaction = require("./Interaction");
+const MessageContextCommand = require("./MessageContextCommand");
+const Poll = require("./Poll");
+const SlashCommand = require("./SlashCommand");
+const Unixify = require("./Unixify");
+const UserContextCommand = require("./UserContextCommand");
+
+module.exports = {
+  Attachment,
+  Embed,
+  Interaction,
+  MessageContextCommand,
+  Poll,
+  SlashCommand,
+  Unixify,
+  UserContextCommand,
+};
