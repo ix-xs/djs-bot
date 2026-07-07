f687eee5741aad7eff1c4fe8f0531fe3f6980512
Ix-xs <poplomo@outlook.fr>
2026-07-07 13:55:10 +0200

    Add type definitions for UI components

---
+++ types/src/components/$.d.ts
@@ -0,0 +1,17 @@
+export const ActionRow: typeof import("./ActionRow");
+export const Button: typeof import("./Button");
+export const Checkbox: typeof import("./Checkbox");
+export const CheckboxGroup: typeof import("./CheckboxGroup");
+export const Container: typeof import("./Container");
+export const File: typeof import("./File");
+export const FileUpload: typeof import("./FileUpload");
+export const Gallery: typeof import("./Gallery");
+export const Label: typeof import("./Label");
+export const Modal: typeof import("./Modal");
+export const Section: typeof import("./Section");
+export const SelectMenu: typeof import("./SelectMenu");
+export const Separator: typeof import("./Separator");
+export const TextDisplay: typeof import("./TextDisplay");
+export const TextInput: typeof import("./TextInput");
+export const Thumbnail: typeof import("./Thumbnail");
+//# sourceMappingURL=$.d.ts.map
